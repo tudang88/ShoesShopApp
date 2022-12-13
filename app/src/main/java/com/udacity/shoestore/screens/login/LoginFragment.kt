e4334595031c9af02cd59320496a4dd176d2eaa0
tudang <dang.huu.tu88@gmail.com>
2022-12-13 21:05:02 +0900

    add layout for Welcome Fragment

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -19,7 +22,11 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        // binding OnClick
+        binding.loginButton.setOnClickListener{view: View -> view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment2())}
+
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+package com.udacity.shoestore.screens.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+
+}
