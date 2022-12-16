9fd077ffb8e88bb7b465de02f317716a9a7ed9b4
tudang <dang.huu.tu88@gmail.com>
2022-12-16 09:29:29 +0900

    use two-way data binding for login view model

--- app/src/main/java/com/udacity/shoestore/models/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/models/LoginViewModel.kt
@@ -1,15 +1,14 @@
 package com.udacity.shoestore.models
 
 import android.util.Log
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
-    private var _userName: String = ""
-    val userName: String
-        get() = _userName
-    private var _password: String = ""
+    var userName: String = ""
+    var password: String = ""
     private val _authFinishedEvent = MutableLiveData<Boolean>()
     val authFinishedEvent: LiveData<Boolean>
         get() = _authFinishedEvent
@@ -18,25 +17,11 @@ class LoginViewModel : ViewModel() {
         get() = _loginFailedEvent
 
     /**
-     * this function will be binding from layout
-     */
-    fun onUserNameEditChange(email: CharSequence, start: Int, before: Int, count: Int) {
-        _userName = email.toString()
-    }
-
-    /**
-     * this function will be binding from layout
-     */
-    fun onPasswordEditChange(email: CharSequence, start: Int, before: Int, count: Int) {
-        _password = email.toString()
-    }
-
-    /**
      * The authentication procedure will connect to backend database
      */
     fun authentication() {
         Log.i("LoginViewModel", "call authentication()")
-        if (connectUserDataBase(_userName, _password)) {
+        if (connectUserDataBase(userName, password)) {
             _authFinishedEvent.postValue(true)
         } else {
             _loginFailedEvent.postValue(true)
@@ -50,7 +35,7 @@ class LoginViewModel : ViewModel() {
     }
 
     private fun connectUserDataBase(userName: String, password: String): Boolean {
-        // Todo: T.B.D
+        // dummy implementation
         if (userName == "" || password == "") {
             return false
         }
